558c846789957416f283b8ed0f6bc7f639967a70
K.Kobayashi <kobayashi@sonicgarden.jp>
2025-03-29 07:37:22 +0900

    入力を受け付けてファイルに書き出すところまで

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+import System.IO
+import System.Environment
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+import System.FilePath (takeExtension)
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case args of
+        [filePath] -> do
+            putStrLn ">"
+            input <- T.getLine
+            T.writeFile filePath input
+            putStrLn $ "Text has been written to " ++ filePath
+        _ -> putStrLn "Usage: stack run <file-path>"
