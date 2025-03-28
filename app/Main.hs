a3d0695dd701d3cc81f04d75a6cc7f8ab1eac4ae
K.Kobayashi <kobayashi@sonicgarden.jp>
2025-03-29 07:57:45 +0900

    入力した文字列の前行に現在日時を記載するように

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,10 @@
 
 import System.IO
 import System.Environment
-import Data.Text (Text, append)
+import Data.Text (Text, append, pack)
 import qualified Data.Text.IO as T
 import System.FilePath (takeExtension)
+import Data.Time (getZonedTime, formatTime, defaultTimeLocale)
 
 main :: IO ()
 main = do
@@ -21,6 +22,10 @@ loop filePath = do
     if input == "exit"
         then putStrLn "Exiting..."
         else do
+            T.appendFile filePath ("\n")
+            currentTime <- getZonedTime
+            let timeStamp = formatTime defaultTimeLocale "%Y-%m-%d(%a) %H:%M:%S" currentTime
+            T.appendFile filePath (pack timeStamp `append` "\n")
             T.appendFile filePath (input `append` "\n")
             putStrLn $ "Text has been written to " ++ filePath
             loop filePath
